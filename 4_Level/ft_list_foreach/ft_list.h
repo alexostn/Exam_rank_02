0fd6dc698e95eb6922f99df534c76d6ec252cdc9
alexostn <157902890+alexostn@users.noreply.github.com>
2024-09-28 23:24:48 +0200

    Create ft_list.h

---
+++ 4_Level/ft_list_foreach/ft_list.h
@@ -0,0 +1,12 @@
+#ifndef	ANY_NAME
+# define ANY_NAME
+
+typedef struct    s_list
+{
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *));
+
+#endif
