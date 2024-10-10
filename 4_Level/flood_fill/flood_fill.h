b64e61b5dc37d38d615a683552101d6c6a1c9010
alexostn <157902890+alexostn@users.noreply.github.com>
2024-10-10 21:45:04 +0200

    Create flood_fill.h

---
+++ 4_Level/flood_fill/flood_fill.h
@@ -0,0 +1,12 @@
+#ifndef DOES_NOT_MATTER_THE_NAME
+# define DOES_NOT_MATTER_THE_NAME
+
+  typedef struct  s_point
+  {
+    int           x;
+    int           y;
+  }               t_point;
+
+void  flood_fill(char **tab, t_point size, t_point begin);//mention the function
+
+#endif
