5fe254360d99973237242fc708a2f3e80b067ce2
alexostn <157902890+alexostn@users.noreply.github.com>
2024-10-11 23:09:20 +0200

    Update union.c

--- 2_Level/union.c
+++ 2_Level/union.c
@@ -11,12 +11,14 @@ int  main(int ac, char **av)
       {
         if (!seen[(unsigned char)av[1][i]])
           write(1, &av[1][i], 1);
+        seen[(unsigned char)av[2][j]] = 1;
         i++;
       }
     while(av[2][j])
       {
         if (!seen[(unsigned char)av[2][j]])
             write(1, &av[2][j], 1);
+        seen[(unsigned char)av[2][j]] = 1;
         j++;
       }    
   }
