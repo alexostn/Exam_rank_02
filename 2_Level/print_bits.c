d918d658cef8a78cd192918396543ba1f8aa791f
alexostn <157902890+alexostn@users.noreply.github.com>
2024-10-18 14:50:51 +0200

    Update print_bits.c

--- 2_Level/print_bits.c
+++ 2_Level/print_bits.c
@@ -18,11 +18,11 @@ Example, if you pass 2 to print_bits, it will print "00000010"
 void	print_bits(unsigned char octet)
 {
   int  i = 8;
-  unsigned char  bit;
+  unsigned char  res;
   
 while(i--)
   {
   res = (octet >> i & 1) + '0';
-  write(1, &bit, 1);
+  write(1, &res, 1);
   }
 }
