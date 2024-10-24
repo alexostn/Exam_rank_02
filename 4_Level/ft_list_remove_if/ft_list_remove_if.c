53982f66efebc32d1df1fc5ae0ce0ad3f1f2a7ce
alexostn <157902890+alexostn@users.noreply.github.com>
2024-10-24 16:25:43 +0200

    Update ft_list_remove_if.c

--- 4_Level/ft_list_remove_if/ft_list_remove_if.c
+++ 4_Level/ft_list_remove_if/ft_list_remove_if.c
@@ -48,7 +48,27 @@ typedef struct      s_list
 }                   t_list;
 $>
 */
+//FIRST SHORT SOLUTION
+#include <stdlib.h>
+#include "ft_list.h"
 
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	if (begin_list == NULL || *begin_list == NULL)
+		return;
+	if ((*cmp)((*begin_list)->data, data_ref) == 0)
+	{
+		t_list *tmp = *begin_list;
+		*begin_list = (*begin_list)->next;
+		free(tmp);
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+	else
+		ft_list_remove_if(&(*begin_list)->next, data_ref, cmp);
+}
+
+
+//SECOND SOLUTION
 #include <stdlib.h>
 #include "ft_list.h"
 
